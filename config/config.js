33e51df050c866dcc8f955bbddfd8cc445da2267
sharnsimon <sharnjackson2002@gmail.com>
2023-04-01 15:41:12 +0530

    sequelize

---
+++ config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+CONFIG = {}
+CONFIG.db_host = process.env.DB_HOST;
+CONFIG.db_port = process.env.DB_PORT;
+CONFIG.db_name = process.env.DB_NAME;
+CONFIG.db_user = process.env.DB_USER;
+CONFIG.db_password = process.env.DB_PASSWORD;
